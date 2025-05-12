310b84edfcc31b70526e023456c46eb3d21d51aa
Tharusha Jayasooriya <90096115+Tharusha-dev@users.noreply.github.com>
2025-05-12 17:57:28 +0530

    Updated not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,3 @@
-'use client'
-
-import {useEffect, useState} from 'react'
 
 /**
  * Not Found component.
@@ -8,19 +5,13 @@ import {useEffect, useState} from 'react'
  * @see https://nextjs.org/docs/app/api-reference/file-conventions/not-found
  */
 export default function NotFound() {
-  const [referer, setReferer] = useState('')
 
-  useEffect(() => {
-    // Get the referer from document.referrer on the client side
-    if (typeof document !== 'undefined') {
-      setReferer(document.referrer)
-    }
-  }, [])
+
 
   return (
     <>
       <h1 className="text-center">404 - Not Found</h1>
-      {referer && <p className="text-center text-red-500">{referer}</p>}
+    
     </>
   )
 }
