d0cb15baa2010f665ba31ab90794129e7d76b862
tharusha-dev <tharushajayasooriya.dev004@gmail.com>
2025-05-12 16:53:48 +0530

    ui improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import getAllPosts from '@/lib/queries/getAllPosts'
-import getPageBySlug from '@/lib/queries/getPageBySlug'
 import {Post} from '@/lib/types'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -11,24 +10,16 @@ import {notFound} from 'next/navigation'
  * @see https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#pages
  */
 export default async function Home() {
-  // Fetch homepage from WordPress.
-  const homepage = await getPageBySlug('sample-page')
-
   // Fetch posts from WordPress.
   const posts = await getAllPosts()
 
   // No data? Bail...
-  if (!posts || !posts.length || !homepage) {
+  if (!posts || !posts.length) {
     notFound()
   }
 
   return (
     <main className="flex flex-col gap-8">
-      <article>
-        <h1 dangerouslySetInnerHTML={{__html: homepage.title}} />
-        <div dangerouslySetInnerHTML={{__html: homepage.content}} />
-      </article>
-
       <aside>
         <h2>Latest Posts</h2>
         <div className="flex flex-wrap gap-8">
